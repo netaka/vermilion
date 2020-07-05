54c22f544a259e093904ea3f741a7f1626602fd2
netaka <developer@netaka.net>
2020-07-05 22:18:22 +0900

    Read a VRM file

--- vrmi/src/main.rs
+++ vrmi/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
+fn main() -> io::Result<()> {
+    let path = "test.vrm";
+    let mut f = File::open(path)?;
+    let mut buffer = Vec::new();
+    
+    f.read_to_end(&mut buffer)?;
+
+    println!("filename: {}", path);
+    println!("size: {}", buffer.len());
+
+    Ok(())
 }
