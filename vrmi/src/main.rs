e2a7a9ce1cb20fda1748241a1a44193d32f96ce9
netaka <developer@netaka.net>
2020-07-06 20:56:35 +0900

    Parse glTF file header

--- vrmi/src/main.rs
+++ vrmi/src/main.rs
@@ -2,6 +2,24 @@ use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 
+use nom::bytes::complete::tag;
+use nom::number::complete::*;
+use nom::{error::ErrorKind, Err};
+
+struct GLTFHeader<'a> {
+    magic: &'a [u8],
+    version: u32,
+    length: u32,
+}
+
+fn parse_header(input : &[u8]) -> Result<GLTFHeader, Err<(&[u8], ErrorKind)>> {
+    let (input, magic) = tag(b"\x67\x6c\x54\x46")(input)?;
+    let (input, version) = le_u32(input)?;
+    let (_input, length) = le_u32(input)?;
+
+    Ok(GLTFHeader{magic, version, length})
+}
+
 fn main() -> io::Result<()> {
     let path = "test.vrm";
     let mut f = File::open(path)?;
@@ -10,7 +28,17 @@ fn main() -> io::Result<()> {
     f.read_to_end(&mut buffer)?;
 
     println!("filename: {}", path);
-    println!("size: {}", buffer.len());
 
+    match parse_header(&buffer) {
+        Ok(header) => {
+            println!("header:");
+            println!("  magic: {}", String::from_utf8(header.magic.to_vec()).unwrap());
+            println!("  version: {}", header.version);
+            println!("  length: {}", header.length);
+        }
+        Err(err) => {
+            println!("{:?}", err);
+        }
+    };
     Ok(())
 }
